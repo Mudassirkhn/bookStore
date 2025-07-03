21fc5bb66684b1c12cdb435aedeff5df2c3176fb
Mudassir Khan <mmkhan213@gmail..com>
2025-07-03 17:33:44 +0530

    nishan change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/my-orders" element={<MyOrders />} />
 
-          // ✅ Book Detail Page
+          // ✅ Book Detail
           <Route path="/book/:id" element={<BookDetail />} />
           <Route
             path="/course"
